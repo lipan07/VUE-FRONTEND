ccc5ade041ab21055d8ad28f10137a13ea3737f6
lipan07 <lipan.biswas.dev@gmail.com>
2023-11-17 12:12:27 +0530

    API url changes

--- src/http/api.js
+++ src/http/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: 'http://localhost:8000',
 });
 
 export default api;
--- src/http/task-api.js
+++ src/http/task-api.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
-const apiPath = import.meta.env.VITE_API_PATH;
+const apiPath = 'http://localhost:8000/api/v2';
 const resource = apiPath + "/tasks";
 
 export const allTasks = () => api.get(resource);
